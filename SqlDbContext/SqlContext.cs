a70cb6d002255af220c5db9e26046ef86c1cbbe0
Administrator <Administrator@LAB1B-18>
2025-01-29 10:16:16 +0500

    Add project files.

---
+++ SqlDbContext/SqlContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop4DVDS.SqlDbContext
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+
+        }
+    }
+}
